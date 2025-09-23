e0100e6afd8cb093ce0508814c3fb4bd0e729f2c
ammarafadia <ammarafadia07@gmail.com>
2025-09-23 20:37:16 +0800

    pt 2

---
+++ kelas/pertemuan-1/main.py
@@ -0,0 +1,63 @@
+#  print("Hello World!")
+
+#  nama = "Daffa"
+#  Nama = "Nabil"
+# angka = '123'
+
+# print(nama)
+# print (type (angka))
+
+# print ("""Halo
+# saya sedang belajar
+# menggunakan bahasa python""")
+
+# print("halo""saya""daffa")
+# print("halo"+ ' ' + "daffa")
+
+# teks = 'saya suka belajar apd'
+# print (teks [3])
+# print (teks [0:4])
+
+Hujan = False
+Panas = True
+
+hasil = Hujan == Panas
+print(hasil)
+
+# prodi = ['Informatika', True, 1, 'TeknikSipil']
+
+# print (prodi[3])
+
+# tuple = ('Lian', 19, True, 'APD')
+# print(tuple[2])
+
+# biodata = {'nama' : 'daffa',
+#            'kelas' : 'B',
+#            'umur' : '19'}
+
+# print(biodata['nama'])
+# print(biodata['kelas'])
+# print(biodata['umur'])
+
+# angka = 123
+# angka1 = str(angka)
+
+# print(type(angka))
+# print(type(angka1))
+
+# nama = input("Masukkan nama anda : ")
+# print(nama)
+
+# operator1 = not (1 > 5)
+# print(operator1)
+
+# nilai1 = 85.5
+# nilai2 = 91.0
+# nilai3 = 77.5
+# nilai4 = 88.0
+
+# nilai_siswa = [nilai1,nilai2,nilai3,nilai4]
+
+# print(nilai_siswa)
+# total = sum(nilai_siswa)
+# print(total)
